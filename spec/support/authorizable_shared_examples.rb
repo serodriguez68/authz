51a62389995a75fb25047c75e197c46603f8c158
Sergio Rodriguez <se.rodriguez68@gmail.com>
2018-12-01 16:00:53 +1100

    Add authorizable shared examples to repo

---
+++ spec/support/authorizable_shared_examples.rb
@@ -0,0 +1,33 @@
+RSpec.shared_examples "authorizable" do
+
+  describe 'associations' do
+    it { should have_many(:role_grants) }
+    it { should have_many(:roles).through(:role_grants) }
+    it { should have_many(:business_processes).through(:roles) }
+    it { should have_many(:controller_actions).through(:business_processes) }
+  end
+
+  describe 'controller action clearance checking' do
+    let!(:authorizable) { create(:user) }
+    let!(:r) {create :opinionated_pundit_role}
+    let!(:bp) {create :opinionated_pundit_business_process}
+    let!(:ca) {create :opinionated_pundit_controller_action}
+    let!(:bphca) {create :opinionated_pundit_business_process_has_controller_action,
+                   controller_action: ca, business_process: bp}
+    let!(:rhpb) {create :opinionated_pundit_role_has_business_process,
+                  business_process: bp, role: r}
+    let!(:rg) {create :opinionated_pundit_role_grant, role: r, rolable: authorizable}
+
+    it 'should have clearance for an assigned controller action' do
+      clearance = authorizable.clear_for?(controller: ca.controller, action: ca.action)
+      expect(clearance).to be true
+    end
+
+    it 'should not have clearance for an unassigned controller action' do
+      clearance = authorizable.clear_for?(controller: ca.controller, action: "#{ca.action}_foo")
+      expect(clearance).to be false
+    end
+
+  end
+
+end
