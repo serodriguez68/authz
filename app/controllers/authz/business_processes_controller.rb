b4e4324c8e32dcc99ffe9d239e18f29ddc1dc675
pinzonjulian <julianpinzoneslava@gmail.com>
2019-01-07 11:20:00 -0500

    adds create and edit base to business processes

--- app/controllers/authz/business_processes_controller.rb
+++ app/controllers/authz/business_processes_controller.rb
@@ -14,18 +14,42 @@ module Authz
     end
 
     def new
+      @business_process = BusinessProcess.new
     end
 
     def create
+      @business_process = BusinessProcess.new(business_process_params)
+      if @business_process.save
+        redirect_to business_process_path(@business_process)
+      else
+        render 'new'
+      end
     end
 
     def edit
+      @business_process = BusinessProcess.find(params[:id])
     end
 
     def update
+      @business_process = BusinessProcess.find(params[:id])
+      if @business_process.update(business_process_params)
+        redirect_to business_process_path(@business_process)
+      else
+        render 'edit'
+      end
     end
 
     def destroy
+    end
+
+    private
+
+    def business_process_params
+      params.require(:business_process)
+            .permit(
+              :name,
+              :description
+            )
     end
   end
 end
