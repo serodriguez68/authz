ccfbae838e988261495bfef98d77ec7eaf0a66b4
pinzonjulian <julianpinzoneslava@gmail.com>
2019-01-07 11:11:39 -0500

    adds create and edit base roles

--- app/controllers/authz/roles_controller.rb
+++ app/controllers/authz/roles_controller.rb
@@ -15,18 +15,40 @@ module Authz
     end
 
     def new
+      @role = Role.new
     end
 
     def create
+      @role = Role.new(role_params)
+      if @role.save
+        redirect_to role_path(@role)
+      else
+        render 'new'
+      end
     end
 
     def edit
+      @role = Role.find(params[:id])
     end
 
     def update
+      @role = Role.find(params[:id])
+      if @role.update(role_params)
+        redirect_to role_path(@role)
+      else
+        render 'edit'
+      end
     end
 
     def destroy
+    end
+
+    def role_params
+      params.require(:role)
+            .permit(
+              :name,
+              :description,
+            )
     end
   end
 end
