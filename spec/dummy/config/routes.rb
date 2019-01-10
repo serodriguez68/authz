4a7b1d7020c742bf1cf9874f5c205f31ea91fc51
Sergio Rodriguez <se.rodriguez68@gmail.com>
2019-01-10 15:43:18 +1100

    Hide Ratings and Cleanup unnecessary views

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   resources :clearances, except: [:show]
   resources :cities, except: [:show]
   resources :reports
-  resources :ratings, only: [:index, :new, :create, :destroy]
+  # resources :ratings, only: [:index, :new, :create, :destroy]
   resources :announcements
 end
