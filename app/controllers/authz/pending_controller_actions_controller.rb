d9ae4f4b3d93e43ca08e0f196b1b0c9762fd0059
pinzonjulian <julianpinzoneslava@gmail.com>
2019-01-27 23:01:20 -0500

    fixes indentation

--- app/controllers/authz/pending_controller_actions_controller.rb
+++ app/controllers/authz/pending_controller_actions_controller.rb
@@ -4,7 +4,7 @@ module Authz
   class PendingControllerActionsController < ApplicationController
     def index
       @pending_controller_actions = ControllerAction.pending
-                                                        .sort_by { |ca| ca[:controller] }
+                                                    .sort_by { |ca| ca[:controller] }
     end
   end
 end
