2a5884296dc9e16c8414a97d7594a530ab3c21e3
Sergio Rodriguez <se.rodriguez68@gmail.com>
2019-01-31 20:15:27 +1100

    remove unnecessary comments from migration file

--- db/migrate/20181129091325_create_authz_business_process_has_controller_actions.rb
+++ db/migrate/20181129091325_create_authz_business_process_has_controller_actions.rb
@@ -1,21 +1,5 @@
 class CreateAuthzBusinessProcessHasControllerActions < ActiveRecord::Migration[5.2]
   def change
-    # create_table :authz_business_process_has_controller_actions do |t|
-    #   t.integer :controller_action_id, index: { name: 'authz_bphca_controller_action_index' }
-    #   t.integer :business_process_id,  index: { name: 'authz_bphca_business_process_index' }
-    #
-    #   t.timestamps
-    # end
-    #
-    # add_foreign_key :authz_business_process_has_controller_actions,
-    #                 :authz_controller_actions,
-    #                 column: :controller_action_id
-    #
-    # add_foreign_key :authz_business_process_has_controller_actions,
-    #                 :authz_business_processes,
-    #                 column: :business_process_id
-
-
     create_table :authz_business_process_has_controller_actions do |t|
       t.references :authz_controller_action,
                    index: { name: 'authz_bphca_controller_action_index' },
